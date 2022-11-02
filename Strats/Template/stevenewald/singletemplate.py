0af17649ec67e6e2aa2e277cc9089c48419877cd
stevenewald <stevenewald6@gmail.com>
2022-11-02 10:41:42 -0500

    New

--- Strats/Template/stevenewald/singletemplate.py
+++ Strats/Template/stevenewald/singletemplate.py
@@ -10,22 +10,23 @@ class templateStrategy():
   A base strategy that is used to explain how to properly develop a strategy.
   """
   
-  def __init__(self, metrics, initvalues):
+  def __init__(self, stockInfo):
     """
-    Metrics - the actual information you need to track. If this is a specific algorithm, you can hard-code it and remove that argument.
-    Initvalues - the initial values of your arguments. 
+    stockInfo - dictionary of stock prices
     """
-    self.data = zip(metrics,initvalues)
+    self.data = stockInfo
     self.time = time.time()
     self.orders = []
     self.ticks = 0
-    self.trend = "Up"
+    self.trends = {}
+    for stock in self.data:
+      self.trends[stock] = "Up"
 #YOU WILL WANT A FULL SUITE OF SETTER AND GETTER METHODS!
 
-  def getData(self):
-    return self.data
+  def getPrice(self, stock) -> dict:
+    return self.data[stock]['price']
 
-  def getTime(self):
+  def getTime(self) -> time:
     return self.time
 
   def getOrders(self) -> array:
@@ -34,11 +35,14 @@ class templateStrategy():
   def getTicks(self) -> int:
     return self.ticks
 
-  def getTrend(self) -> str:
-    return self.trend
+  def getTrend(self, stock) -> str:
+    return self.trends[stock]
 
-  def setTrend(self, trend: str) -> None:
-    self.trend = trend
+  def setStock(self, stock, info) -> None:
+    self.data[stock] = info
+
+  def setTrend(self, stock, trend: str) -> None:
+    self.trends[stock] = trend
   
 def clear_orders(self):
     """
@@ -47,31 +51,30 @@ def clear_orders(self):
     print(f"Trashing %d orders.", len(self.orders))
     self.orders = []
     
-def update(self, data):
+def update(self, updatedData):
     """
     Will be called on every tick to update the algorithm state and output buys/sells.
     """
     self.ticks += 1
-    #Ingest Data
-    updates = zip(data.keys(), data.values())
-    for metric, information in updates:
-      self.data[metric] = information
-      
     self.clear_orders()
+    #Ingest Data
+    for stock in updatedData:
+      # retrieve data to store previous price
+      prevPrice = self.getPrice(stock)
 
-    # retrieve data to store previous price
-    prevPrice = self.getData()['price']
+      # Update new stock info
+      self.setStock(stock, updatedData)
 
-    # use new data to store updated price
-    newPrice = data['price']
+      # use new data to store updated price
+      newPrice = updatedData[stock]['price']
 
-    # if the new price is ge old and we had a downward trend, reverse position
-    if newPrice>=prevPrice and self.getTrend()=="Down":
-      self.setTrend("Up")
-      self.orders.append("BUY")
-    # otherwise, if new price is less than old and our previous trend was upwards, reverse position
-    elif newPrice<prevPrice and self.getTrend()=="Up":
-      self.setTrend("Down")
-      self.orders.append("SELL")
+      # if the new price is ge old and we had a downward trend, reverse position
+      if newPrice>=prevPrice and self.getTrend(stock)=="Down":
+        self.setTrend(stock, "Up")
+        self.orders.append((stock, "BUY"))
+      # otherwise, if new price is less than old and our previous trend was upwards, reverse position
+      elif newPrice<prevPrice and self.getTrend(stock)=="Up":
+        self.setTrend(stock, "Down")
+        self.orders.append((stock, "SELL"))
     return self.orders
     
